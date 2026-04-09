07b94762e9d825d1ad8f26939483839ab6934ec4
bitamez <bitamez.feedback@gmail.com>
2026-04-09 01:29:55 -0700

    Add SSL mode require to fix database crashes on Render and update email config

--- bus_ticket_backend/settings.py
+++ bus_ticket_backend/settings.py
@@ -66,6 +66,9 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', '6543'),
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
 
@@ -104,4 +107,4 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = f"EXPRESSGO <{EMAIL_HOST_USER}>"
